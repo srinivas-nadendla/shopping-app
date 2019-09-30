284dd8ba01995f08c7a87e3213264e68f2c59d1b
Srinivasarao Nadendla <srinivasarao.nadendla@publicissapient.com>
2019-09-30 12:08:08 +0530

    using useState hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import PLP from './components/ProductListingPage';
@@ -7,28 +7,18 @@ import PDP from './components/ProductDetailPage';
 import items from './core/constants';
 import { StoreContextProvider } from './core/store-context';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      storeContext: {
-        items: items,
-        cartItems: []
-      }
-    };
-  }
-  render() {
-    return (
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={props => <StoreContextProvider value={this.state.storeContext}><PLP items={items} /></StoreContextProvider>}></Route>
-          <Route path="/cart" render={(props) => <StoreContextProvider value={this.state.storeContext}><Cart items={items} {...props} /></StoreContextProvider>}></Route>
-          <Route path={`/pdp/:id`} render={(props) => <StoreContextProvider value={this.state.storeContext}><PDP items={items} {...props} /> </StoreContextProvider>} />
-        </Switch>
-      </BrowserRouter>
-    )
-  }
+const App = () => {
+  const [storeContext] = useState({ items: items, cartItems: [] });
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={props => <StoreContextProvider value={storeContext}><PLP items={items} /></StoreContextProvider>}></Route>
+        <Route path="/cart" render={(props) => <StoreContextProvider value={storeContext}><Cart  {...props} /></StoreContextProvider>}></Route>
+        <Route path={`/pdp/:id`} render={(props) => <StoreContextProvider value={storeContext}><PDP {...props} /> </StoreContextProvider>} />
+      </Switch>
+    </BrowserRouter>
+  )
 
 }
 
